4395cc77091d00b5c22ffe1f030e5f605b9885b1
Dima <dorogovd@yandex.ru>
2022-12-21 19:27:30 +0300

    Initial commit
    - 1st code example added (sum all numbers in an array and return the sum)

--- divedeAndRule/main.swift
+++ divedeAndRule/main.swift
@@ -9,3 +9,11 @@ import Foundation
 
 print("Hello, World!")
 
+func sum(array: [Int]) -> Int {
+   var total = 0
+    for x in array {
+        total += x
+    }
+    return total
+}
+print(sum(array: [1, 2, 3, 4]))
