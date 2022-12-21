e3712a2205ce050c77f040fe5ee444c88243d848
Dima <dorogovd@yandex.ru>
2022-12-21 21:10:35 +0300

    sumRecursion & countRecursion
    - added 1st version of sumRecursion & countRecursion methods

--- divedeAndRule/main.swift
+++ divedeAndRule/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 print("Hello, World!")
 
-func sum(array: [Int]) -> Int {
+func sum(array: [Int]) -> Int { // сумма чисел в массиве с помощью цикла
    var total = 0
     for x in array {
         total += x
@@ -17,3 +17,23 @@ func sum(array: [Int]) -> Int {
     return total
 }
 print(sum(array: [1, 2, 3, 4]))
+
+
+func sumRecursion(array: [Int]) -> Int { // упражнение 4.1 "Напишите код для функции sum"
+    if array == [] {
+        return 0
+    } else {
+        return array.reduce(0, +)
+    }
+}
+print(sumRecursion(array: [2, 4, 6]))
+
+
+func countRecursion(array: [Int]) -> Int { // упражнение 4.2 "Напишите рекурсивную функцию для подсчета элементов в списке"
+    if array == [] {
+        return 0
+    } else {
+        return array.count
+    }
+}
+print(countRecursion(array: [2, 4, 6]))
